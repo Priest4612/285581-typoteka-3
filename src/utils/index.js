d4ab0082a2d012403721f84acf4b35433c6c6cb9
priest4612 <prie4612@gmail.com>
2020-06-10 23:11:06 +0300

    Добавил общие методы

---
+++ src/utils/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const getRandomInt = require(`./get-random-int`);
+const shuffle = require(`./shuffle`);
+
+const Utils = {
+  getRandomInt,
+  shuffle,
+};
+
+module.exports = {
+  Utils,
+};
