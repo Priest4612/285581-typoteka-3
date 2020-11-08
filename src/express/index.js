415e90d1a88cbd0bce939653d31edba3f3527441
priest4612 <prie4612@gmail.com>
2020-11-08 21:16:26 +0300

    Настроил шаблонизатор

--- src/express/index.js
+++ src/express/index.js
@@ -1,15 +1,27 @@
 'use strict';
+const path = require(`path`);
 const express = require(`express`);
 
 const settings = require(`../../settings`);
-
 const {mainRouter} = require(`./routes/main-routes`);
+const {HttpStatusCode} = require(`../constants.js`);
 
 
 const DEFAULT_PORT = settings.DEFAULT_PORT;
+const PROJECT_DIR = settings.PROJECT_DIR;
+const PUBLIC_DIR = settings.PUBLIC_DIR;
+const TEMPLATES_DIR = settings.TEMPLATES_DIR;
+
 const app = express();
 
+app.set(`view engine`, `pug`);
+app.set(`views`, path.resolve(PROJECT_DIR, TEMPLATES_DIR));
+
+app.use(express.static(path.resolve(PROJECT_DIR, PUBLIC_DIR)));
+
 app.use(`/`, mainRouter);
+app.use((req, res) => res.status(HttpStatusCode.NOT_FOUND).render(`errors/404`));
+// app.use((error, req, res, next) => res.status(HttpStatusCode.ERROR).render(`errors/500`));
 
 app.listen(DEFAULT_PORT,
     () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`));
