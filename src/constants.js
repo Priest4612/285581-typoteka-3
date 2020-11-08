7b66dd7d74225a84ce96ec20b7cf3d8a7200bc95
priest4612 <prie4612@gmail.com>
2020-11-08 21:11:50 +0300

    Изменил константы

--- src/constants.js
+++ src/constants.js
@@ -6,17 +6,18 @@ const ExitCode = {
   SUCCESS: 0,
   ERROR: 1,
 };
-const HttpCode = {
+const HttpStatusCode = {
   OK: 200,
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
   FORBIDDEN: 403,
   UNAUTHORIZED: 401,
+  ERROR: 500
 };
 
 module.exports = {
   USER_ARGV_INDEX,
   DEFAULT_COMMAND,
   ExitCode,
-  HttpCode,
+  HttpStatusCode,
 };
