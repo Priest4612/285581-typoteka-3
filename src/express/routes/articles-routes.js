3686cbf19f2a49f4d6d00f210133e72003879a43
priest4612 <prie4612@gmail.com>
2020-11-23 23:44:27 +0300

    Обновил шаблон

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -5,18 +5,25 @@ const {Router} = require(`express`);
 const articlesRouter = new Router();
 const api = require(`../api`).getAPI();
 
+const {getLogger} = require(`../../service/lib/logger`);
+const logger = getLogger({name: `ARTICLES-ROUTER`});
 
 articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles/all-categories`));
 
 articlesRouter.get(`/add`, (req, res) => res.render(`articles/new-post`));
 
 articlesRouter.get(`/edit/:id`, async (req, res) => {
-  const {id} = req.params;
-  const [apiArticleData, apiCategoriesData] = await Promise.all([
-    api.getArticle(id),
-    api.getCategories()
-  ]);
-  res.render(`articles/edit`, {apiArticleData, apiCategoriesData});
+  try {
+    const {id} = req.params;
+    const [apiArticleData, apiCategoriesData] = await Promise.all([
+      api.getArticle(id),
+      api.getCategories()
+    ]);
+    res.render(`articles/edit`, {apiArticleData, apiCategoriesData});
+  } catch (error) {
+    res.render(`errors/404`);
+    logger.error(error);
+  }
 });
 
 articlesRouter.get(`/:id`, (req, res) => res.render(`articles/post`));
