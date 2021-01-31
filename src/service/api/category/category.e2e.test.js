7f2cbd56f9a11592b4f6d3494b906c36da7b2f50
priest4612 <prie4612@gmail.com>
2021-01-31 23:47:13 +0300

    Добавил тест для categoryApi

--- src/service/api/category/category.e2e.test.js
+++ src/service/api/category/category.e2e.test.js
@@ -2,32 +2,58 @@
 
 const express = require(`express`);
 const request = require(`supertest`);
+const Sequelize = require(`sequelize`);
 
-const {HttpStatusCode} = require(`../../../constants`);
+const {initDB} = require(`../../lib/init-db`);
 const category = require(`./category`).categoryRouter;
 const DataService = require(`../../data-service`).CategoryService;
 
-const mockData = require(`./category-mock-data.json`);
+const {HttpStatusCode} = require(`../../../constants`);
 
-const app = express();
-category(app, new DataService(mockData));
+
+const {roles, users, categories, articles} = require(`./category-mock-data`);
+
+const createAPI = async () => {
+  const mockDB = new Sequelize(`sqlite::memory:`, {logging: false});
+  await initDB(mockDB, {data: {
+    roles,
+    users,
+    categories,
+    articles,
+  }});
+  const app = express();
+  app.use(express.json());
+  category(app, new DataService(mockDB));
+  return app;
+};
 
 
 describe(`API return category list`, () => {
-  let response;
+  let app; let response;
 
   beforeAll(async () => {
+    app = await createAPI();
     response = await request(app)
       .get(`/categories`);
   });
 
   test(`Status code 200`, () => expect(response.statusCode).toBe(HttpStatusCode.OK));
 
-  test(`Return list of 4 categories`, () => expect(response.body.length).toBe(5));
+  test(`Return list of 9 categories`, () => expect(response.body.length).toBe(9));
 
-  test(`Category names are "Кино", "Музыка", "Железо", "Без рамки", "За жизнь"`,
-      () => expect(response.body).toEqual(
-          expect.arrayContaining([`Кино`, `Музыка`, `Железо`, `Без рамки`, `За жизнь`])
+  test(`Category names are "Журналы", "Игры", "Животные"`,
+      () => expect(response.body.map((it) => it.name)).toEqual(
+          expect.arrayContaining([
+            `Музыка`,
+            `Деревья`,
+            `Разное`,
+            `Кино`,
+            `За жизнь`,
+            `IT`,
+            `Программирование`,
+            `Без рамки`,
+            `Железо`
+          ])
       )
   );
 });
